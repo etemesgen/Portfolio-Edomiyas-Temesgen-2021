b0dc983fd2eda0b01e4caf68bd619592ddf6e8cb
Edomiyas <temesgen.edomiyas@yahoo.com>
2021-08-24 03:00:35 +0200

    Integrated Tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template> 
-  <div id="app">
-    <p id="title">Edomiyas <strong>Temesgen</strong></p>
-    <nav>
-      <NavLinks url="#" text="Home"/>
-      <NavLinks url="#" text="About me"/>
-      <NavLinks url="#" text="Projects"/>
-      <NavLinks url="#" text="Contact me"/>
+  <div id="app" class="flex-1">
+    <p id="title" class="text-white mt-9 ml-8">Edomiyas <strong>Temesgen</strong></p>
+    <nav class="-mt-7 ml-80 text-xl text-green-300 flex space-x-20">
+      <NavLinks url="#" text="Home" class="hover:text-white"/>
+      <NavLinks url="#" text="About me" class="hover:text-white"/>
+      <NavLinks url="#" text="Projects" class="hover:text-white"/>
+      <NavLinks url="#" text="Contact me" class="hover:text-white"/>
     </nav>
-    <div id="social-media">
-      <a href="#"><img :src="image"  alt="linkedin" id="linkedin"></a>
-      <p id="linkedintext">LinkedIn</p>
-      <a href="#"><img src="./assets/github.png" alt="github" id="github"></a>
-      <p id="githubtext">GitHub</p>
+    <div id="social-media" class="flex justify-end space-x-4 -mt-7 mr-10">
+      <a href="#"><img :src="image"  alt="linkedin" id="linkedin" class="max-w-full w-7"></a>
+      <p id="linkedintext" class="text-white mt-1">LinkedIn</p>
+      <a href="#"><img src="./assets/github.png" alt="github" id="github" class="max-w-full w-7"></a>
+      <p id="githubtext" class="text-white mt-1">GitHub</p>
     </div>
   </div>
 </template>
@@ -35,61 +35,9 @@ export default {
 
 
 <style>
-/*@import url('http://fonts.cdnfonts.com/css/rockwell-nova');*/
+@import url('http://fonts.cdnfonts.com/css/rockwell-nova');
 
 body{
-  background-color: #4E5A6B;
-  margin: 0;
-  padding: 0;
   font-family: 'Rockwell Nova';
-}
-
-#app {
-  display: flex;
-}
-
-#title{
-  margin: 2em 0rem 0rem 5rem;
-  font-size: 1.2rem;
-  color: #FFFFFF;
-}
-
-nav {
-  margin: 2.3rem 0rem 0rem 6rem;
-  font-size: 1.2rem;
-}
-
-nav a{
-  text-decoration: none;
-  color: #9FFAC9;
-  margin-left: 4rem;
-}
-
-nav a:hover{
-  color: #FFFFFF;
-}
-
-#linkedin{
-  max-width: 100%;
-  width: 1.7rem;
-  margin: 2.2rem 0rem 0rem 7rem;
-  background-color: #4E5A6B;
-}
-
-#linkedintext{
-  margin: -1.8rem 0rem 0rem 9.2rem;
-  color: #FFFFFF;
-}
-
-#github{
-  max-width: 100%;
-  width: 1.7rem;
-  margin: -2rem 0rem .8rem 16rem;
-  background-color: #4E5A6B;
-}
-
-#githubtext{
-  margin: -2.6rem 0rem 0rem 18.5rem;
-  color: #FFFFFF;
 }
 </style>
