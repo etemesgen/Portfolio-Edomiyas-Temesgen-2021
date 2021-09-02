e120fb2195498716941ce55562e08ac22394cdaf
Edomiyas <temesgen.edomiyas@yahoo.com>
2021-09-02 03:40:30 +0200

    Worked on navbar responsivity

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template> 
   <div id="app" class="flex-1">
-    <p id="title" class="text-white mt-9 ml-8">Edomiyas <strong>Temesgen</strong></p>
-    <nav class="-mt-7 ml-80 text-xl text-green-300 flex space-x-20 md:">
-      <NavLinks url="#" text="Home" class="hover:text-white"/>
-      <NavLinks url="#" text="About me" class="hover:text-white"/>
-      <NavLinks url="#" text="Projects" class="hover:text-white"/>
-      <NavLinks url="#" text="Contact me" class="hover:text-white"/>
+    <p id="title" class="text-white text-xl mt-9 ml-8">Edomiyas <strong>Temesgen</strong></p>
+    <button id="hamburger" class="flex text-green-200 text-3xl ml-80 -mt-7 lg:hidden" v-on:click="toggleNavbar()">
+      <i class="fa fa-bars"></i>
+    </button>
+    <nav v-bind:class="{'hidden': !showMenu, 'flex': showMenu}" class="text-green-200 text-xl mt-4 flex-col items-center lg:inline">
+      <NavLinks url="#" text="Home" class="hover:text-black lg:flex-row hover:text-white"/>
+      <NavLinks url="#" text="About me" class="hover:text-black lg:flex-row hover:text-white"/>
+      <NavLinks url="#" text="Projects" class="hover:text-black lg:flex-row hover:text-white"/>
+      <NavLinks url="#" text="Contact me" class="hover:text-black lg:flex-row hover:text-white"/>
     </nav>
     <div id="social-media" class="flex justify-end space-x-4 -mt-7 mr-10">
-      <a href="#"><img :src="image"  alt="linkedin" id="linkedin" class="max-w-full w-7"></a>
-      <p id="linkedintext" class="text-white mt-1">LinkedIn</p>
-      <a href="#"><img src="./assets/github.png" alt="github" id="github" class="max-w-full w-7"></a>
-      <p id="githubtext" class="text-white mt-1">GitHub</p>
+      <a href="#"><img :src="image"  alt="linkedin" id="linkedin" class="hidden max-w-full w-7 lg:flex"></a>
+      <p id="linkedintext" class="hidden text-white mt-1 lg:flex">LinkedIn</p>
+      <a href="#"><img src="./assets/github.png" alt="github" id="github" class="hidden max-w-full w-7 lg:flex"></a>
+      <p id="githubtext" class="hidden text-white mt-1 lg:flex">GitHub</p>
     </div>
-    <p id="dev" class="bg-green-300 w-56 p-1 font-bold text-xl text-center mt-28 ml-36 rounded">Full-Stack Developer</p>
-    <p class="text-white text-3xl ml-36 mt-7">If you never failed,</p>
-    <p class="text-white text-3xl ml-72">you never tried anything new.</p>
-    <img class="w-40 float-right mr-64 -mt-32" :src="me" alt="me">
+    <p id="dev" class="bg-green-200 w-56 p-1 font-bold text-xl text-center ml-16 mt-28 rounded lg:ml-36">Full-Stack Developer</p>
+    <p class="text-white text-xl mr-4 mt-7 lg:text-3xl ml-36">If you never failed,</p>
+    <p class="text-white text-xl lg:text-3xl ml-72">you never tried anything new.</p>
+    <img class="w-32 lg:w-48 float-right mr-52 -mt-36" :src="me" alt="me">  <!-- Mobile first responsive, define mobile size by default-->
   </div>
 </template>
 
@@ -33,7 +36,13 @@ export default {
   data(){
     return{
       image: image,
-      me: me
+      me: me,
+      showMenu: false
+    }
+  },
+  methods: {
+    toggleNavbar: function(){
+      this.showMenu = !this.showMenu;
     }
   }
 }
